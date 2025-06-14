6ec01dba63bb8caf8b2ea6997bb7f65b991e3817
rajatAgrawalHub <rajat.agrawal2022@vitstudent.ac.in>
2025-06-14 18:59:07 +0530

    port update

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 })
 .then(() => {
-  const PORT = process.env.PORT || 5000;
+  const PORT =  5000;
   app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 })
 .catch(err => {
